c0ce37903ae0b847034c79500db93498680b0abd
Nabil Shaikh <Nabil.Shaikh.Nabil@gmail.com>
2026-03-13 00:42:09 +1100

    bugfix: bucket name
    buckname is globally unique. refactor ingest logic. upload new truck data csv.

--- ingest_from_s3.sql
+++ ingest_from_s3.sql
@@ -1,11 +1,14 @@
--- This SQL script is executed by the Lambda whenever a new CSV file arrives in S3.
--- Arguments: {bucket_name}, {file_key} are injected by the Lambda at runtime.
+-- 1. Set the Context
+USE DATABASE SNOWFLAKE_LEARNING_DB;
+USE SCHEMA NABILSHAIKH_LOAD_DATA_FROM_CLOUD;
 
--- 1. Create a temporary Stage to point to the new file
-CREATE OR REPLACE TEMPORARY STAGE SNOWFLAKE_LEARNING_DB.NABILSHAIKH_LOAD_DATA_FROM_CLOUD.temp_s3_stage
-  URL = 's3://{bucket_name}/{file_key}';
+-- 2. Create a temporary Stage pointing to the BUCKET (folder)
+CREATE OR REPLACE TEMPORARY STAGE temp_s3_stage
+  URL = 's3://{bucket_name}/'
+  STORAGE_INTEGRATION = s3_integration;
 
--- 2. Ingest the data from S3 into the table (Table must already exist!)
-COPY INTO SNOWFLAKE_LEARNING_DB.NABILSHAIKH_LOAD_DATA_FROM_CLOUD.TRUCK_LOADS
-FROM @SNOWFLAKE_LEARNING_DB.NABILSHAIKH_LOAD_DATA_FROM_CLOUD.temp_s3_stage
+-- 3. Ingest only the specific FILE that triggered the Lambda
+COPY INTO TRUCK_LOADS
+FROM @temp_s3_stage
+FILES = ('{file_key}')
 FILE_FORMAT = (TYPE = CSV, SKIP_HEADER = 1);
